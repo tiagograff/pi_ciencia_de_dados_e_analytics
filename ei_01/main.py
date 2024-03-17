d49315c81633dc97515a8910aff1a2fe0c31df63
tiago <tiagopinzgraffunder@gmail.com>
2024-03-17 15:30:19 -0300

    conclusão do primeiro estudo independente, analise e cruzamente de uma base de dados com python

--- ei_01/main.py
+++ ei_01/main.py
@@ -1,24 +1,58 @@
 import pandas as pd
 
 # ler o excel
-exel_file = 'faculdade/pi_ciencia_de_dados_e_analytics/ei_01/DNRS2022.xlsx'
-df_xlsx = pd.read_excel(exel_file)
-# acessando colunas na planilha do excel
-idades = df_xlsx['IDADEMAE']
-codResMae = df_xlsx['CODOCUPMAE']
-codSexo = df_xlsx['SEXO']
-# variáveis
-menor_idade = idades  # função que retorna o mínimo/menos
-pessoas = []  # array para armazenar as pessoas
+excel_file = 'faculdade/pi_ciencia_de_dados_e_analytics/ei_01/DNRS2022.xlsx'
+df_xlsx = pd.read_excel(excel_file)
 
-# iterar cada idades em idades, com o index i
+# acessar colunas da planilha
+idades = df_xlsx['IDADEMAE']
+codResMae = df_xlsx['CODMUNRES']
+codSexo = df_xlsx['SEXO']
+
+# variáveis
+# função que retorna o menor valor
+menor_idade = idades.min()
+# arrays para armazenar valores
+pessoas = []
+municipios = []
+sexos = []
+
+# for index e idade em idades
 for i, idade in enumerate(idades):
-    # se a idade for igual a menor idade encontrada
+    # se a idade for igual a menor encontrada
     if idade == menor_idade:
-        # colocar no array de pessoas
-        pessoas.append((codResMae[i], codSexo[i]))
-# printar resultados
-print('a menor idade é:', menor_idade)
-# para cada residência e sexo em pessoas printar
-for codRes, codSexo in pessoas:
-    print('código do município:', codRes, '\nsexo do bebê:', codSexo)
+        # armazena informações nos arrays
+        pessoas.append(idade)
+        municipios.append(codResMae[i])
+        sexos.append(codSexo[i])
+
+# imprimir informações obtidas
+# itera simultaneamente sobre múltiplas listas
+for idade, municipio, sexo in zip(pessoas, municipios, sexos):
+    print('idade da mãe:', idade, '\ncódigo do município',
+          municipio)
+
+# Ler o CSV com os códigos dos municípios e seus nomes
+file_csv = 'faculdade/pi_ciencia_de_dados_e_analytics/ei_01/tb_municip.csv'
+# lendo arquivo csv e colocando o delimitador padrão como ; e tirando a coluna index
+df_csv = pd.read_csv(file_csv, delimiter=';', index_col=False)
+# para codigos de municpio em municipios
+for index, codigo_municipio in enumerate(municipios):
+    # reservar nome do municipio onde loc [linhas, colunas]
+    nome_municipio = df_csv.loc[df_csv['CO_MUNICIP']
+                                == codigo_municipio, 'DS_NOME'].iloc[index]
+    # printar municipio
+    print('municipio:', nome_municipio)
+    index += 1
+
+# baseado nos documentos Estrutura SINASC_rev_NOVO.doc
+for sexo in sexos:
+    if sexo == 0:
+        print('sexo: indefinido')
+    elif sexo == 1:
+        print('sexo: masculino')
+    elif sexo == 2:
+        print('sexo: feminino')
+    else:
+        print('não encontrado')
+    index += 1
--- ei_01/tempCodeRunnerFile.py
+++ ei_01/tempCodeRunnerFile.py
@@ -1,22 +1,4 @@
-# ler o excel
-# exel_file = 'faculdade/pi_ciencia_de_dados_e_analytics/ei_01/DNRS2022.xlsx'
-# df_xlsx = pd.read_excel(exel_file)
-# # acessando colunas na planilha do excel
-# idades = df_xlsx['IDADEMAE']
-# codResMae = df_xlsx['CODOCUPMAE']
-# codSexo = df_xlsx['SEXO']
-# # variáveis
-# menor_idade = idades  # função que retorna o mínimo/menos
-# pessoas = []  # array para armazenar as pessoas
+file_csv = 'faculdade/pi_ciencia_de_dados_e_analytics/ei_01/tb_municip.csv'
+df_csv = pd.read_csv(file_csv, index_col=False)
 
-# # iterar cada idades em idades, com o index i
-# for i, idade in enumerate(idades):
-#     # se a idade for igual a menor idade encontrada
-#     if idade == menor_idade:
-#         # colocar no array de pessoas
-#         pessoas.append((codResMae[i], codSexo[i]))
-# # printar resultados
-# print('a menor idade é:', menor_idade)
-# # para cada residência e sexo em pessoas printar
-# for codRes, codSexo in pessoas:
-#     print('código do município:', codRes, '\nsexo do bebê:', codSexo)
+print(df_csv['CO_MUNICIP'].head(5))
